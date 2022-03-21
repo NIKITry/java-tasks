e9d97bb7c6c53c9a2f611180b1e8f05cf43711d9
nikitabandurin2002@gmail.com <)M8YM%XO]UU#)Vk>
2022-03-21 17:38:01 +0300

    All tests done except priority and custom

--- src/main/java/ru/mail/polis/homework/analyzer/TextAnalyzer.java
+++ src/main/java/ru/mail/polis/homework/analyzer/TextAnalyzer.java
@@ -15,23 +15,26 @@ package ru.mail.polis.homework.analyzer;
  * 2 тугрика + (2 тугрика за каждый фильтр + 1 тугрик за тест на свой фильтр) ИТОГО 11
  */
 public interface TextAnalyzer {
-
     static TextAnalyzer createTooLongAnalyzer(long maxLength) {
-        return null;
+        return new TooLongAnalyzer(maxLength);
     }
 
     static TextAnalyzer createSpamAnalyzer(String[] spam) {
-        return null;
+        return new SpamAnalyzer(spam);
     }
 
     static TextAnalyzer createNegativeTextAnalyzer() {
-        return null;
+        return new NegativeAnalyzer();
     }
 
     /**
      * придумать свой фильтр
      */
     static <T> TextAnalyzer createCustomAnalyzer(T something) {
+        return null;
+    }
+
+    default FilterType analyze(String text) {
         return null;
     }
 }
